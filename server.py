482df24cc3edd433cc9fca7ff879e9d8150c3cb9
caseymccullough <kcmccullough@gmail.com>
2021-11-27 13:40:11 -0500

    two points button works, increment value button does not

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, session
+from flask.globals import request
 app = Flask(__name__)
 app.secret_key = 'keep it secret, keep it safe'
 
@@ -9,6 +10,21 @@ def default():
    else:
       session['count'] += 1
    return render_template('index.html')
+
+@app.route('/two')
+def two_points():
+   if "count" not in session:
+      session['count'] = 0
+   else:
+      session['count'] += 2
+      print("UP BY TWO!!")
+   return render_template('index.html')
+
+@app.route('/increment')
+def increment ():
+   print("post info: ")
+   print(request.form)
+   return redirect('/')
 
 @app.route('/reset')
 def reset():
